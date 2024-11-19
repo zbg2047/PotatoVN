74607fbbfb769d89336290d336158fd486bca061
goldenpotato137 <goldenpotato137@gmail.com>
2024-11-20 00:23:48 +0800

    Revert "doc: 引入vuetify3组件库，更新一起开发的index界面"
    This reverts commit 9b29df136b8195357dbd6f3a80c32b7cc572cca2.

--- GalgameManager.Doc/.vitepress/theme/index.ts
+++ GalgameManager.Doc/.vitepress/theme/index.ts
@@ -2,24 +2,11 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import MsStoreBadge from './components/MsStoreBadge.vue'
-import Card from './components/Card.vue'
-import { createVuetify } from 'vuetify'
-import 'vuetify/styles'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     // 注册自定义全局组件
     app.component('MsStoreBadge', MsStoreBadge)
-    app.component('Card', Card)
-
-    const vuetify = createVuetify({
-      components,
-      directives,
-    })
-
-    app.use(vuetify)
   }
 } satisfies Theme
