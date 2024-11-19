9b29df136b8195357dbd6f3a80c32b7cc572cca2
goldenpotato137 <goldenpotato137@gmail.com>
2024-11-20 00:15:42 +0800

    doc: 引入vuetify3组件库，更新一起开发的index界面

---
+++ GalgameManager.Doc/.vitepress/theme/components/Card.vue
@@ -0,0 +1,50 @@
+﻿<template>
+  <v-card class="mx-auto my-4 custom-card" max-width="700">
+    <v-row>
+      <v-col cols="9">
+        <v-card-title>{{ title }}</v-card-title>
+        <v-card-subtitle>{{ description }}</v-card-subtitle>
+      </v-col>
+      <v-col cols="3" class="d-flex align-center justify-end">
+        <v-img :src="imageLink"></v-img>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'CustomCard',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    imageLink: {
+      type: String,
+      required: true
+    },
+  }
+}
+</script>
+
+<style scoped>
+.v-card-title {
+  font-weight: bold;
+  font-size: 1.5em;
+}
+.v-card-subtitle {
+  font-size: 1em;
+  color: #666;
+  white-space: normal;      
+  word-wrap: break-word;
+}
+.custom-card {
+  padding: 40px 40px 40px 20px;
+}
+
+</style>
--- GalgameManager.Doc/development/index.md
+++ GalgameManager.Doc/development/index.md
@@ -1,1 +1,37 @@
 # 一起开发 PotatoVN！
+**加入我们，一起打造更好的Visual Novel管理工具！**
+
+我们相信开源的力量以及多样性带来的无限可能。 
+我们的项目门户永远向所有喜欢Galgame、热爱技术、渴望贡献的朋友敞开大门。
+无论您是编程新手、资深开发者还是Galgame领域大神，您的参与都是我们的荣幸。
+
+我们鼓励并支持每一位成员的贡献。无论你的技能水平如何，
+只要你有兴趣和热情，就可以加入我们！
+
+以下是一些你可以参与的方面：
+
+## :book: 文档维护
+
+<Card
+title="文档维护"
+description="文档是用户了解和使用产品的第一步。加入我们，帮助完善文档。无论是添加新的内容，还是更新现有信息，您的贡献都是极其宝贵的。"
+image-link="./images/HTML.png"
+/>
+
+本文档是由 [VitePress](https://vitepress.vuejs.org/) 构建的，要编写文档，只需使用 Markdown 语法即可。
+
+## :computer: 客户端开发
+
+<Card
+title="客户端开发"
+description="如果你对客户端编程感兴趣，可以参与修复现有的bug或添加新功能。你的代码将直接影响数以万计的用户体验。"
+image-link="./images/CSharp.png"
+/>
+
+## :globe_with_meridians: 服务端开发
+
+<Card
+title="服务端开发"
+description="PotatoVN具有一个可以自行部署的后端平台用于跨电脑同步游戏信息。 为了让平台运行更加高效与安全，我们需要持续优化后端服务。你可以帮助我们改进服务器性能，或者开发新的后台功能。"
+image-link="./images/DockerLogo.png"
+/>
