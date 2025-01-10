67ea9abb4f835741db75d86fa4deb4ec42a07a11
zbg2047 <zbg2047@gmail.com>
2025-01-10 09:11:55 +0800

    feat(app): 游戏编辑页增加发行时间

--- GalgameManager/Services/GalgameCollectionService/GalgameCollectionService.cs
+++ GalgameManager/Services/GalgameCollectionService/GalgameCollectionService.cs
@@ -273,7 +273,7 @@ public partial class GalgameCollectionService : IGalgameCollectionService
         galgame.RssType = phraser.GetPhraseType();
         galgame.Id = tmp.Id;
         galgame.Description.Value = tmp.Description.Value;
-        if (tmp.Developer != Galgame.DefaultString)
+        if (tmp.Description != Galgame.DefaultString)
             galgame.Description.Value = tmp.Description.Value;
         if (tmp.Developer != Galgame.DefaultString)
             galgame.Developer.Value = tmp.Developer.Value;
@@ -301,7 +301,7 @@ public partial class GalgameCollectionService : IGalgameCollectionService
         }
         galgame.Characters = tmp.Characters;
         galgame.ImagePath.Value = await DownloadHelper.DownloadAndSaveImageAsync(galgame.ImageUrl) ?? Galgame.DefaultImagePath;
-        galgame.ReleaseDate = tmp.ReleaseDate.Value;
+        galgame.ReleaseDate.Value = tmp.ReleaseDate.Value;
         galgame.LastFetchInfoTime = DateTime.Now;
         return galgame;
     }
